e558f4950a70d1ad4202a831c593ad0cc186fb54
Santiago <santigilma@gmail.com>
2023-09-05 11:58:50 -0400

    Initial Commit

---
+++ CafeCanela/CafeCanelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeCanelaApp.swift
+//  CafeCanela
+//
+//  Created by Santiago Gil on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct CafeCanelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
